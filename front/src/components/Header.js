8eb58a6cf6dbb2356d1a73bffe8e7e9731005872
Kaayn <leo.dimartinopro@gmail.com>
2020-08-31 14:13:31 +0200

    add logo

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,13 +1,15 @@
 import React from "react";
-
+import Logo from "../assets/img/Logo.png";
 import "./Header.css";
 
 function Header() {
   return (
     <div className="header">
       <div className="navbar">
-        <a href="/">Dream Pizz</a>
-        <a href="/contact">Contact</a>
+        <img className="logo" src={Logo} alt="Logo of DreamPizz"></img>
+        <a className="tilte" href="/">
+          Dream Pizz
+        </a>
       </div>
       <br />
     </div>
