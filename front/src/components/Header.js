e045926f3e828d8065b9d74a8edd8c8f4450e04e
Léo DI MARTINO <47184542+Kaayn@users.noreply.github.com>
2020-08-31 15:07:02 +0200

    Navbar (#1)

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,12 +1,19 @@
 import React from "react";
-
+import Logo from "../assets/img/Logo.png";
 import "./Header.css";
 
 function Header() {
   return (
     <div className="header">
       <div className="navbar">
-        <a href="/">Dream Pizz</a>
+        <a href="/accueil">
+          <img className="logo" src={Logo} alt="Logo of DreamPizz"></img>
+        </a>
+        <div>
+          <a className="jsp" href="/menu">
+            Menu
+          </a>
+        </div>
       </div>
       <br />
     </div>
