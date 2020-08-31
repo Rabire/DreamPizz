ee6e6a9d1b778c6667c5f84f0f85528c18758ca8
Rabire <rabireh@outlook.fr>
2020-08-31 17:23:42 +0200

    color fix

--- front/src/components/Footer.js
+++ front/src/components/Footer.js
@@ -9,15 +9,17 @@ function Footer() {
   return (
     <div className="footer">
       <div className="contact">
-        <h2>Contact</h2>
+        <h2 className=" txt-white">Contact</h2>
         <div className="container">
           <div className="addressContainer">
-            <h3>DreamPizz</h3>
-            <p className="addressText">88 Rue de Gerland - 69007 - Lyon</p>
-            <p>Près du métro Place Jean Jaurès</p>
+            <h3 className=" txt-white">DreamPizz</h3>
+            <p className="addressText  txt-white">
+              88 Rue de Gerland - 69007 - Lyon
+            </p>
+            <p className=" txt-white">Près du métro Place Jean Jaurès</p>
             <br />
-            <p className="phoneMail">+33 4 12 34 56 78</p>
-            <p className="phoneMail">dreampizz@pizza.com</p>
+            <p className="phoneMail txt-white">+33 4 12 34 56 78</p>
+            <p className="phoneMail txt-white">dreampizz@pizza.com</p>
           </div>
           <div>
             <img
@@ -29,9 +31,9 @@ function Footer() {
         </div>
       </div>
       <div>
-        <h2> Réseaux sociaux</h2>
+        <h2 className=" txt-white">Réseaux sociaux</h2>
         <div className="addressContainer">
-          <p className="networkText">
+          <p className="networkText txt-white">
             Rejoignez-nous sur nos différents réseaux !
           </p>
           <div className="networksContainer">
